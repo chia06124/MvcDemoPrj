02612b7952d2876c9e2e6f7f7b8b22e5924d8c5e
Lily <lilytu01520@outlook.com>
2020-06-24 15:07:17 +0800

    login_bundle_commit

--- MvcDemoPrj/MvcDemoPrj/App_Start/BundleConfig.cs
+++ MvcDemoPrj/MvcDemoPrj/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace MvcDemoPrj
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/loginjs").Include(
+                        "~/Content/vendor/jquery/jquery.js"
+                        , "~/Content/vendor/bootstrap/js/bootstrap.bundle.js"
+                        , "~/Content/vendor/jquery-easing/jquery.easing.js"
+                        , "~/Content/js/sb-admin-2.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +32,9 @@ namespace MvcDemoPrj
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/content/logincss").Include(
+                      "~/Content/vendor/fontawesome-free/css/all.css",
+                      "~/Content/css/sb-admin-2.css"));
         }
     }
 }
