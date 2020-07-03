4dbbda46e805b532b34f240e54495f6c56059ccd
Lily <lilytu01520@outlook.com>
2020-07-03 21:55:48 +0800

    調整RWD

--- MvcDemoPrj/MvcDemoPrj/App_Start/BundleConfig.cs
+++ MvcDemoPrj/MvcDemoPrj/App_Start/BundleConfig.cs
@@ -20,24 +20,19 @@ namespace MvcDemoPrj
                         , "~/Content/vendor/jquery-easing/jquery.easing.js"
                         , "~/Content/js/sb-admin-2.js"));
 
-
-            bundles.Add(new ScriptBundle("~/bundles/indexjs").Include(
-                        "~/Content/vendor/jquery/jquery.min.js"
-                        , "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js"
-                        , "~/Content/vendor/jquery-easing/jquery.easing.min.js"
-                        , "~/Content/js/sb-admin-2.min.js"
-                        , "~/Content/vendor/chart.js/Chart.js"
-                        , "~/Content/js/demo/chart-area-demo.js"
-                        , "~/Content/js/demo/chart-pie-demo.js"
-                        , "~/Content/vendor/datatables/jquery.dataTables.min.js"
-                        , "~/Content/vendor/datatables/dataTables.bootstrap4.min.js"
+             bundles.Add(new ScriptBundle("~/bundles/indexjs").Include(
+                        "~/Content/vendor/jquery/jquery.js"
+                        , "~/Content/vendor/bootstrap/js/bootstrap.bundle.js"
+                        , "~/Content/vendor/jquery-easing/jquery.easing.js"
+                        , "~/Content/js/sb-admin-2.js"
+                        , "~/Content/vendor/datatables/jquery.dataTables.js"
+                        , "~/Content/vendor/datatables/dataTables.bootstrap4.js"
                         , "~/Content/js/demo/datatables-demo.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/datepickerjs").Include(
                         "~/Scripts/moment.js"
                         , "~/Scripts/bootstrap-datetimepicker.js"
                         ,"~/Scripts/bootstrap-datepicker.js"));
-
 
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好實際執行時，請使用 http://modernizr.com 上的建置工具，只選擇您需要的測試。
@@ -56,11 +51,11 @@ namespace MvcDemoPrj
                       "~/Content/css/sb-admin-2.css"));
             bundles.Add(new StyleBundle("~/content/indexcss").Include(
                       "~/Content/vendor/fontawesome-free/css/all.css",
-                      "~/Content/vendor/fontawesome-free/css/all.min.css",
-                      "~/Content/css/sb-admin-2.min.css",
-                      "~/Content/vendor/datatables/dataTables.bootstrap4.min.css"));
+                      "~/Content/css/sb-admin-2.css",
+                      "~/Content/vendor/datatables/dataTables.bootstrap4.css"));
             bundles.Add(new StyleBundle("~/content/datepickercss").Include(
                       "~/Content/bootstrap-datepicker.css"));
+            
         }
     }
 }
