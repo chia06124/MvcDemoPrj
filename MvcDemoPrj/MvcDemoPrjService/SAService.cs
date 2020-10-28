c3997efdc57cce0c25e0de3738f1cc3ae32e0141
Lily <lilytu01520@outlook.com>
2020-10-28 22:37:55 +0800

    commit SAService and SysCodeMapService

---
+++ MvcDemoPrj/MvcDemoPrjService/SAService.cs
@@ -0,0 +1,22 @@
+﻿using MvcDemoPrj.SQLModel.Interface;
+using MvcDemoPrj.SQLModel.Models;
+using MvcDemoPrj.SQLModel.Repository;
+using MvcDemoPrjService.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcDemoPrjService
+{
+   public class SAService: ISAService
+    {
+        private readonly IRepository<SA_User> saRepository = new GenericRepository<SA_User>();
+        public List<SA_User> GetAll()
+        {
+            var query = (from row in saRepository.GetAll() select row).ToList();
+            return query;
+        }
+    }
+}
---
+++ MvcDemoPrj/MvcDemoPrjService/SysCodeMapService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MvcDemoPrj.SQLModel.Models;
+using MvcDemoPrj.SQLModel.Interface;
+using MvcDemoPrj.SQLModel.Repository;
+using MvcDemoPrjService.Interface;
+
+namespace MvcDemoPrjService
+{
+    public class SysCodeMapService:ISysCodeMapService
+    {
+        private readonly IRepository<sysCodeMap> sysCodeMapRepository = new GenericRepository<sysCodeMap>();
+        public List<sysCodeMap> GetAll()
+        {
+            var query = (from row in sysCodeMapRepository.GetAll() select row).ToList();
+            return query;
+        }
+    }
+}
